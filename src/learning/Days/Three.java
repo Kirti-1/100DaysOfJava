8e5246f877d9bfeb100892d233932d6e37fe3fc6
Kirti Arora <Kirti-1@users.noreply.github.com>
2024-04-09 23:57:44 +0530

    Threads Day - 1

--- src/learning/Days/Three.java
+++ src/learning/Days/Three.java
@@ -35,14 +35,17 @@ public class Three extends Thread {
 		//Let's create another thread and then interrupt it after some time.
 		Thread thread = new Thread(()->{
 			for(int i=0;i<10;i++) {
+				String tname = Thread.currentThread().getName();
+				String state = Thread.currentThread().getState().toString();
+				
 				System.out.print(" . ");
 				try {
-					Thread.currentThread().sleep(250);
+					Thread.sleep(250);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 					return;
 				}
+				System.out.println(tname + i +  "  <<< with state >>> " + state);
 			}
 		});
 		
@@ -50,15 +53,46 @@ public class Three extends Thread {
 		thread.start();
 		System.out.println("Main Thread!");
 		
+
+		String mname = Thread.currentThread().getName();
+		String mstate = Thread.currentThread().getState().toString();
+
+		String tname = thread.getName();
+		String state = thread.getState().toString();
 		try {				
 			Thread.sleep(1000);
 			//interrupted thread after 1 sec the output will be - . . . . 
-			thread.interrupt();
+
+			//after 1 sec the state of thread-0 was in blocked state as it has been blocked as sleep time main thread got finished, now main thread will execute.
+			
+			System.out.println(tname  + "  <<< with state >>> " + state);
+			System.out.println(mname  + "  <<< with state >>> " + mstate);
+			
+			
+//			thread.interrupt();
 			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	
+		
+		/* Output of above code -- with thread name and its state
+		 Main Thread!
+		 . Thread-00  <<< with state >>> RUNNABLE
+		 . Thread-01  <<< with state >>> RUNNABLE
+		 . Thread-02  <<< with state >>> RUNNABLE
+		 . Thread-0  <<< with state >>> BLOCKED
+		main  <<< with state >>> RUNNABLE
+		
+		Main Thread Ended!
+		Thread-03  <<< with state >>> RUNNABLE
+		 . Thread-04  <<< with state >>> RUNNABLE
+		 . Thread-05  <<< with state >>> RUNNABLE
+		 . Thread-06  <<< with state >>> RUNNABLE
+		 . Thread-07  <<< with state >>> RUNNABLE
+		 . Thread-08  <<< with state >>> RUNNABLE
+		 . Thread-09  <<< with state >>> RUNNABLE
+		 */
 		
 		System.out.println();		
 		System.out.println("Main Thread Ended!");
